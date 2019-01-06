d68f4d0290e97efe5c5882fb6b6c990aef5ade84
jameseclement <jameseclement@gmail.com>
2019-01-06 15:14:01 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
+
+## defines method which accepts a string called sentence1
 def reverse_each_word(sentence1)
+  ## sentence1.split turns the string into an array, where each word is an element of the array. No spaces.
  sentence_array = sentence1.split
- sentence_array.collect { |word| word.reverse!}
+ sentence_array.collect { |word|
+ 
+ ## .reverse! reverses each word "in place", not creating new
+ word.reverse!
+ }
+ ## .join(" ") takes the elements of the array, joins them as a string with a space between each 
   sentence_array.join(" ")
 end
